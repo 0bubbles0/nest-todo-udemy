78cb0e9d8ea621ad60c972583a3f44529d31f098
Barbara <appenzeller.barbara@gmail.com>
2022-03-14 23:15:08 +0000

    🥅 Handle Filter Errors with DTO

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus) // Validation
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsString() // Validation
   search?: string;
 }
