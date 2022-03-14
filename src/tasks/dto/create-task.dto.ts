9393b39e0ae10ca629b78d56c0ab367dd52a65e3
Barbara <appenzeller.barbara@gmail.com>
2022-03-14 22:16:38 +0000

    🥅 Error-handling with Global Pipe: createTask empty-fields

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateTaskDto {
+  @IsNotEmpty() // Server-side Validation for title
   title: string;
+
+  @IsNotEmpty() // Server-side Validation for description
   description: string;
 }
