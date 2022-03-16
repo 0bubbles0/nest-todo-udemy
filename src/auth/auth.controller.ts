d757aea0f5d74c428ad108c960203fd7f25b5066
Barbara <appenzeller.barbara@gmail.com>
2022-03-16 18:23:57 +0000

    🔐 Auth: signUp() saves to db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // Post signup
+  // Post to Route /auth/signup
+  @Post("/signup")
+  SignUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
   // Post signin
   // Post signout
   // Protected route
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,16 @@
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    // validate: empty, requirements (username unique, password length/numbersSymbols), error code/message/action
+
+    // password: encrypt, hash, salt
+    // store in database
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
