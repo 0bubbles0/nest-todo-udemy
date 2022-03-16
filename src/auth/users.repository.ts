054c984a40b8f1907787735b62892f32a985dfe8
Barbara <appenzeller.barbara@gmail.com>
2022-03-16 19:15:41 +0000

    🔐 Auth: signUp() password hash salt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,20 +5,23 @@ import {
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
+import * as bcrypt from "bcrypt";
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    // validate: empty, requirements (username unique, password length/numbersSymbols), error code/message/action
 
-    // password: encrypt, hash, salt
-    // store in database
-    const user = this.create({ username, password });
+    // Password: encrypt, hash, salt
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Store in database
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
 
-      // Error handling
+      // Validate & Error handling: empty, requirements (username unique, password length/numbersSymbols), error code/message/action
     } catch (error) {
       // this pg error.code is for username duplication error:
       if (error.code == "23505") {
